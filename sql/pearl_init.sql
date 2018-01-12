aea7622c4dba9705bb2a666096c8ccdbf071edf4
Yulianto Adi <thinkyoel@gmail.com>
2018-01-12 10:45:36 +0700

    2.0.7

--- sql/pearl_init.sql
+++ sql/pearl_init.sql
@@ -1,1 +1,2 @@
 update `core_config_data` set `value`=5 where `scope`='stores' and `scope_id`=1 and `path`='design/theme/theme_id';
+update `core_config_data` set `value`=5 where `scope`='default' and `scope_id`=0 and `path`='design/theme/theme_id';
