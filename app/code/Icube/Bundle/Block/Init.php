bc9c0c12702f13b61d4e6b1752ef2d1dcebd22c0
root <root@mgt-dev-70>
2018-07-23 11:47:15 +0000

    Create module for implementing bundle JS via r.js in Magento

---
+++ app/code/Icube/Bundle/Block/Init.php
@@ -0,0 +1,28 @@
+<?php
+namespace Icube\Bundle\Block;
+
+class Init extends \Magento\Framework\View\Element\Template
+{
+    protected $_assetRepo;
+	protected $helper;
+	
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\View\Asset\Repository $assetRepo,
+        \Icube\CustomStyle\Helper\Data $helper
+    ) {
+        parent::__construct($context);
+        $this->_assetRepo = $assetRepo;
+        $this->helper = $helper;
+    }
+
+    public function getAssetRepo($file)
+    {
+        return $this->_assetRepo->getUrl($file);
+    }
+    
+    public function isEnableJsBundling()
+    {
+        return $this->helper->getConfigValue('icube_bundle/bundle_js/bundle_js_enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+}
