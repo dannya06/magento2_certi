b5449a77ce75ea43386202ed5b1b97788bf73f6c
Yulianto Adi <yuliantoadi@icube.us>
2019-02-12 10:53:39 +0700

    install cron manager

---
+++ app/code/EthanYehuda/CronjobManager/view/adminhtml/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    shim: {
+        'EthanYehuda_CronjobManager/js/timeline/timeline': {
+            'deps': ['EthanYehuda_CronjobManager/js/lib/knockout/bindings/virtual-foreach']
+        }
+    }
+};
