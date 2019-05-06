67646511c66400a440cbe545714e30b36acf6a7b
yuliantoadi <yulianto@icube.us>
2019-05-06 20:32:57 +0700

    Update msi.sql

--- msi.sql
+++ msi.sql
@@ -1,7 +1,7 @@
 UPDATE cataloginventory_stock_status css
 left JOIN cataloginventory_stock_item csi ON css.`product_id` = csi.`product_id`
 SET css.`qty` = csi.`qty`, css.`stock_status` = csi.`is_in_stock`
-WHERE csi.`qty` IS NOT NULL
+WHERE csi.`qty` IS NOT NULL;
 
 INSERT INTO inventory_source_item(`source_code`,`sku`,`quantity`,`status`)
 select * from (
@@ -10,4 +10,4 @@ from cataloginventory_stock_item cs
 left join catalog_product_entity cp
 on cs.`product_id`=cp.`entity_id`
 where cp.`type_id`="simple") as c
-ON DUPLICATE KEY UPDATE `quantity`=c.`quantity`, `status`=c.`status`
+ON DUPLICATE KEY UPDATE `quantity`=c.`quantity`, `status`=c.`status`;
