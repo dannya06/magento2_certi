a71dce58e46721148e11cd00c55ec234d04a3230
yulianto adi <yulianto@icube.us>
2019-05-08 05:18:51 +0000

    updated zero.sh

--- zero.sh
+++ zero.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # magento 2 minimum downtime deployment
 
 site_dir=/home/mage2user/site
@@ -7,19 +8,6 @@ release_version=$(date +%Y%m%d%H%M%S)
 
 printf "Deployment is starting\n"
 printf "=====================================\n"
-printf "Delete old pre code\n"
-printf "=====================================\n";
-
-rm -rf $site_dir$releases/pre_code
-mkdir $site_dir$releases/pre_code
-cp -R $site_dir$current/. $site_dir$releases/pre_code/
-
-printf "=====================================\n"
-printf "Fetch pre code from related branch\n"
-printf "=====================================\n"
-
-cd $site_dir$releases/pre_code && git fetch origin
-cd $site_dir$releases/pre_code && git checkout -b $release_version origin/master
 
 printf "=====================================\n"
 printf "Deployment in pre code folder\n"
@@ -46,29 +34,52 @@ fi
 
 
 printf "=====================================\n"
-printf "Fetch pre code from related branch\n"
+printf "Fetch production code from related branch\n"
 printf "=====================================\n"
 
 
 cd $site_dir$current && git fetch origin
-cd $site_dir$current && git checkout -b $release_version origin/master
+
+if [ "$1" = "development" ]
+then
+	printf "=====================================\n"
+	printf "get latest code from develop branch\n"
+	printf "=====================================\n"
+	cd $site_dir$current && git checkout -b $release_version origin/develop
+elif [ "$1" = "production" ]
+then
+	printf "=====================================\n"
+	printf "get latest code from master branch\n"
+	printf "=====================================\n"
+	cd $site_dir$current && git checkout -b $release_version origin/master
+else
+	printf "=====================================\n"
+	printf "no branch\n"
+	printf "=====================================\n"
+fi
 
 printf "=====================================\n"
 printf "Copying file from pre code to production\n"
 printf "=====================================\n"
 
-cd $site_dir$current && composer install
+php $site_dir$current/bin/magento maintenance:enable
 rm -rf $site_dir$current/var/generated/*
 rm -rf $site_dir$current/var/view_preprocessed/*
 rm -rf $site_dir$current/pub/static/*
 cp -r $site_dir$releases/pre_code/generated/* $site_dir$current/generated/
 cp -r $site_dir$releases/pre_code/pub/static/* $site_dir$current/pub/static/
+cd $site_dir$current && composer install
+php $site_dir$current/bin/magento maintenance:disable
+
+php $site_dir$current/bin/magento cache:flush
+php $site_dir$current/bin/magento cache:enable
+sudo service varnish restart
 
 php $site_dir$current/bin/magento setup:db:status
 status=$?
 if [ $status -eq 1 ]; then 
 	php $site_dir$current/bin/magento maintenance:enable
-	php $site_dir$current/bin/magento setup:upgrade
+	php $site_dir$current/bin/magento setup:upgrade --keep-generated
 	sudo service varnish restart
 	php $site_dir$current/bin/magento maintenance:disable
 else
@@ -76,10 +87,6 @@ else
 fi
 
 
-php $site_dir$current/bin/magento cache:flush
-php $site_dir$current/bin/magento cache:enable
-sudo service varnish restart
-
-printf "====================================="
+printf "=====================================\n"
 printf "Deployment is done\n"
 printf "=====================================\n"
