c1a36bd26f8363d5d2d87f2a53ea265840e21f2e
yulianto adi <yulianto@icube.us>
2019-06-21 01:31:47 +0000

    fixed msi fixing and update compose confirm payment

--- msi.sql
+++ msi.sql
@@ -5,9 +5,14 @@ WHERE csi.`qty` IS NOT NULL;
 
 INSERT INTO inventory_source_item(`source_code`,`sku`,`quantity`,`status`)
 select * from (
-select 'default' as source_code,sku,qty as "quantity", cs.`is_in_stock` as "status"
+select 'default' as source_code,cp.sku,(qty + (IFNULL(c.quantity,0))) as "quantity", cs.`is_in_stock` as "status"
 from cataloginventory_stock_item cs
 left join catalog_product_entity cp
 on cs.`product_id`=cp.`entity_id`
+left join (
+select sku,SUM(quantity) as quantity 
+from inventory_reservation
+group by sku) c
+on cp.`sku`=c.`sku`
 where cp.`type_id`="simple" and cs.`qty` IS NOT NULL) as c
 ON DUPLICATE KEY UPDATE `quantity`=c.`quantity`, `status`=c.`status`;
