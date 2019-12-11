9eabecdd719f55356bdb07d4b26702554a76eb24
yuliantoadi <thinkyoel@gmail.com>
2019-12-11 12:02:48 +0700

    remove newest sort

--- app/code/Icube/CatalogSorting/Model/Config.php
+++ app/code/Icube/CatalogSorting/Model/Config.php
@@ -7,7 +7,6 @@ class Config extends \Magento\Catalog\Model\Config
     public function getAttributeUsedForSortByArray()
     {
        $options = [
-              'newest' => __('Newest'),
               'position' => __('Position'),
        				'bestseller' => __('Best Seller'),
        				'most_viewed' => __('Most Viewed'),
