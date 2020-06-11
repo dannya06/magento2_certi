2562b527dbeea1bcc09e41d4e56b67fcd2e4f2c6
Hanif Rizal <hanif@icube.us>
2020-06-11 02:09:27 +0000

    pdf reduce invoice

---
+++ app/code/Icube/Pdf/Model/Pdf/Invoice.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Icube\Pdf\Model\Pdf;
+
+use Magento\Sales\Model\ResourceModel\Order\Invoice\Collection;
+
+/**
+ * Sales Order Invoice PDF model
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Invoice extends \Magento\Sales\Model\Order\Pdf\AbstractPdf
+{
+    public function getPdf($invoices = [])
+    {
+        $this->_beforeGetPdf();
+        $this->_initRenderer('invoice');
+
+        $pdf = new \Zend_Pdf();
+        $this->_setPdf($pdf);
+        $style = new \Zend_Pdf_Style();
+        $this->_setFontBold($style, 10);
+
+        foreach ($invoices as $invoice) {
+            if ($invoice->getStoreId()) {
+                $this->_localeResolver->emulate($invoice->getStoreId());
+                $this->_storeManager->setCurrentStore($invoice->getStoreId());
+            }
+            $page = $this->newPage();
+            $order = $invoice->getOrder();
+            /* Add image */
+            $this->insertLogo($page, $invoice->getStore());
+            /* Add address */
+            $this->insertAddress($page, $invoice->getStore());
+            /* Add head */
+            $this->insertOrder(
+                $page,
+                $order,
+                $this->_scopeConfig->isSetFlag(
+                    self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID,
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                    $order->getStoreId()
+                )
+            );
+            /* Add document text and number */
+            $this->insertDocumentNumber($page, __('Invoice # ') . $invoice->getIncrementId());
+            /* Add table */
+            $this->_drawHeader($page);
+            /* Add body */
+            foreach ($invoice->getAllItems() as $item) {
+                if ($item->getOrderItem()->getParentItem()) {
+                    continue;
+                }
+                /* Draw item */
+                $this->_drawItem($item, $page, $order);
+                $page = end($pdf->pages);
+            }
+            /* Add totals */
+            $this->insertTotals($page, $invoice);
+            if ($invoice->getStoreId()) {
+                $this->_localeResolver->revert();
+            }
+        }
+        $this->_afterGetPdf();
+        return $pdf;
+    }  
+
+    protected function _setFontRegular($object, $size = 7)
+    {
+        $font = \Zend_Pdf_Font::fontWithName(\Zend_Pdf_Font::FONT_HELVETICA); // or FONT_TIMES for serif
+        $object->setFont($font, $size);
+        return $font;
+    }
+
+    protected function _setFontBold($object, $size = 7)
+    {
+        $font = \Zend_Pdf_Font::fontWithName(\Zend_Pdf_Font::FONT_HELVETICA_BOLD); // or FONT_TIMES_BOLD for serif
+        $object->setFont($font, $size);
+        return $font;
+    }
+
+    protected function _setFontItalic($object, $size = 7)
+    {
+        $font = \Zend_Pdf_Font::fontWithName(\Zend_Pdf_Font::FONT_HELVETICA_ITALIC); // or FONT_TIMES_ITALIC for serif
+        $object->setFont($font, $size);
+        return $font;
+    }
+}
