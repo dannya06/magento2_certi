062c4c89e2fd90aee939e9070fb22b98b78ee418
Hanif Rizal <hanif@icube.us>
2020-07-29 07:16:24 +0000

    how to pay

---
+++ app/code/Icube/HowToPay/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+          'Icube_Checkoutpg/template/container/payment/grouping.html':'Icube_HowToPay/template/container/payment/grouping.html'
+        }
+  }
+};
