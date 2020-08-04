ddf5d491ea6b5700119efdd27adceb58d33b2954
Hanif Rizal <hanif@icube.us>
2020-08-04 02:42:48 +0000

    fix how to pay accordion checkout

--- app/code/Icube/HowToPay/view/frontend/requirejs-config.js
+++ app/code/Icube/HowToPay/view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     map: {
         '*': {
-          'Icube_Checkoutpg/template/container/payment/grouping.html':'Icube_HowToPay/template/container/payment/grouping.html'
+          'Icube_Checkoutpg/template/payment.html':'Icube_HowToPay/template/payment.html'
         }
   }
 };
