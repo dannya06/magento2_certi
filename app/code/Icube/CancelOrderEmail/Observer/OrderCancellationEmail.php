0438fd3b452b85aaec8bf176032e51de2d2797ac
yohansyahck <yohansyah@icube.us>
2020-08-05 09:27:12 +0000

    Fixing email copy bcc/separeted

--- app/code/Icube/CancelOrderEmail/Observer/OrderCancellationEmail.php
+++ app/code/Icube/CancelOrderEmail/Observer/OrderCancellationEmail.php
@@ -32,34 +32,71 @@ class OrderCancellationEmail implements ObserverInterface
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $enabled = $this->_scopeConfig->getValue(self::XML_ENABLE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $template = $this->_scopeConfig->getValue(self::XML_TEMPLATE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $sender = $this->_scopeConfig->getValue(self::XML_IDENTITY, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         $copyTo = $this->_scopeConfig->getValue(self::XML_COPY_TO, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         $copyMethod = $this->_scopeConfig->getValue(self::XML_COPY_METHOD, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $sendFrom = $this->_senderResolver->resolve($sender);
 
         if ($enabled == 1) {
             $order = $observer->getData('order');
-            $transport = $this->_transportBuilder
-                ->setTemplateIdentifier($template)
-                ->setTemplateOptions(
-                    [
-                        'area' => 'frontend',
-                        'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,
-                    ]
-                )
-                ->setTemplateVars([
-                    'order' => $order,
-                    'template_subject' => $order->getStoreName().' Order Cancel',
-                    'customername' => $order->getCustomerName(),
-                    'email_content' => 'Your order #'.$order->getIncrementId().' has been canceled.',
-                ])
-                ->setFrom($sendFrom)
-                ->addTo(array($order->getCustomerEmail()))
-                ->addTo(array($copyTo))
-                ->addBcc(array($copyTo))
-                ->getTransport();
-            $transport->sendMessage();
+            try {
+                $this->send($order, $copyTo, $copyMethod);
+            } catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+            }
+            if ($copyMethod == 'copy') {
+                try {
+                    $this->sendCopyTo($order, $copyTo);
+                } catch (\Exception $e) {
+                    $this->logger->error($e->getMessage());
+                }
+            }
+        }
+    }
+
+    public function configureEmailTemplate($order)
+    {
+        $template = $this->_scopeConfig->getValue(self::XML_TEMPLATE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $sender = $this->_scopeConfig->getValue(self::XML_IDENTITY, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $sendFrom = $this->_senderResolver->resolve($sender);
+
+        $this->_transportBuilder
+            ->setTemplateIdentifier($template)
+            ->setTemplateOptions([
+                'area' => 'frontend',
+                'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,
+            ])
+            ->setTemplateVars([
+                'order' => $order,
+                'template_subject' => $order->getStoreName().' Order Cancel',
+                'customername' => $order->getCustomerName(),
+                'email_content' => 'Your order #'.$order->getIncrementId().' has been canceled.',
+            ])
+            ->setFrom($sendFrom);
+    }
+
+    public function send($order, $copyTo, $copyMethod)
+    {
+        $this->configureEmailTemplate($order);
+        $this->_transportBuilder->addTo(array($order->getCustomerEmail()));
+        if (!empty($copyTo) && $copyMethod == 'bcc') {
+            $arrCopy = explode(',', $copyTo);
+            foreach ($arrCopy as $email) {
+                $this->_transportBuilder->addBcc($email);
+            }
+        }
+        $transport = $this->_transportBuilder->getTransport();
+        $transport->sendMessage();
+    }
+
+    public function sendCopyTo($order, $copyTo)
+    {
+        if (!empty($copyTo)) {
+            $arrCopy = explode(',', $copyTo);
+            foreach ($arrCopy as $email) {
+                $this->configureEmailTemplate($order);
+                $this->_transportBuilder->addTo($email);
+                $transport = $this->_transportBuilder->getTransport();
+                $transport->sendMessage();
+            }
         }
     }
 }
