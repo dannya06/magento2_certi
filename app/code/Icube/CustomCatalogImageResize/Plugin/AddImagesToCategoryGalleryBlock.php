18bbaa73c639211646ec5266962df74b8fb47829
yohansyahck <yohansyah@icube.us>
2020-08-14 09:59:41 +0000

    Optimize performance

--- app/code/Icube/CustomCatalogImageResize/Plugin/AddImagesToCategoryGalleryBlock.php
+++ app/code/Icube/CustomCatalogImageResize/Plugin/AddImagesToCategoryGalleryBlock.php
@@ -5,20 +5,24 @@ namespace Icube\CustomCatalogImageResize\Plugin;
 use Magento\Catalog\Block\Product\ListProduct;
 use Magento\Catalog\Model\Product;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\MediaStorage\Service\ImageResize;
+use Icube\CustomCatalogImageResize\Helper\Data;
+use Icube\CustomCatalogImageResize\Service\ImageResize;
 
 class AddImagesToCategoryGalleryBlock
 {
     const XML_ENABLE = 'icube_image_resize/general/enable';
 
     protected $imageResize;
+    protected $resize;
 
     public function __construct(
         ScopeConfigInterface $scopeConfig,
-        ImageResize $imageResize
+        ImageResize $imageResize,
+        Data $resize
     ) {
         $this->_scopeConfig = $scopeConfig;
         $this->imageResize = $imageResize;
+        $this->resize = $resize;
     }
 
     public function afterGetProductDetailsHtml(ListProduct $subject, $result, Product $product) 
@@ -26,8 +30,14 @@ class AddImagesToCategoryGalleryBlock
         $enabled = $this->_scopeConfig->getValue(self::XML_ENABLE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         if ($enabled == 1) {
             $arrImage = array($product->getImage());
+            $paramsAll = $this->imageResize->getViewImages($this->imageResize->getThemesInUse());
+            $params = array_filter($paramsAll, function($elem) {
+                $image_id = $elem['id'];
+                return preg_match("/category/", $image_id);
+            }); 
             foreach ($arrImage as $image) {
-                 $this->imageResize->resizeFromImageName($image);
+                $arrNew[] = $image;
+                $this->resize->resizeImage($arrNew, $params);
             }
             return $result;
         }
--- app/code/Icube/CustomCatalogImageResize/Plugin/AddImagesToGalleryBlock.php
+++ app/code/Icube/CustomCatalogImageResize/Plugin/AddImagesToGalleryBlock.php
@@ -4,33 +4,40 @@ namespace Icube\CustomCatalogImageResize\Plugin;
 
 use Magento\Catalog\Block\Product\View\Gallery;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\MediaStorage\Service\ImageResize;
+use Icube\CustomCatalogImageResize\Service\ImageResize;
+use Icube\CustomCatalogImageResize\Helper\Data;
 
 class AddImagesToGalleryBlock
 {
     const XML_ENABLE = 'icube_image_resize/general/enable';
 
     protected $imageResize;
+    protected $resize;
 
     public function __construct(
         ScopeConfigInterface $scopeConfig,
-        ImageResize $imageResize
+        ImageResize $imageResize,
+        Data $resize
     ) {
         $this->_scopeConfig = $scopeConfig;
         $this->imageResize = $imageResize;
+        $this->resize = $resize;
     }
 
     public function afterGetGalleryImages(Gallery $subject, $images) 
     {
         $enabled = $this->_scopeConfig->getValue(self::XML_ENABLE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         if ($enabled == 1) {
-            $product = $subject->getProduct();
-            $galleryImages = $product->getMediaGalleryImages();
-            if ($galleryImages) {
-                foreach ($product->getMediaGalleryImages() as $image) {
-                    $this->imageResize->resizeFromImageName($image->getFile());
-                }
+            $image = $subject->getProduct();
+            $paramsAll = $this->imageResize->getViewImages($this->imageResize->getThemesInUse());
+            $params = array_filter($paramsAll, function($elem) {
+                $image_id = $elem['id'];
+                return !preg_match("/category/", $image_id);
+            }); 
+            foreach ($image->getMediaGalleryImages() as $key) {
+                $product[] = $key->getFile();
             }
+            $this->resize->resizeImage($product, $params);
             return $images;
         }
         return $images;
