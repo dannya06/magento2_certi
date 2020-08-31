5b124676e589071e59f681082ba0d1136c2ec365
yohansyahck <yohansyah@icube.us>
2020-08-31 09:43:35 +0000

    Fixing Clear Page Cache permission on client role

--- app/code/Icube/CustomStyle/Controller/Adminhtml/Cache/DeployStatic.php
+++ app/code/Icube/CustomStyle/Controller/Adminhtml/Cache/DeployStatic.php
@@ -1,138 +1,32 @@
 <?php
-/**
- *
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
 
 namespace Icube\CustomStyle\Controller\Adminhtml\Cache;
 
-use Magento\Framework\Controller\ResultFactory;
-use Magento\Backend\App\Action;
-use \Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Directory\Helper\Data;
-use Magento\Framework\View\Design\Theme\ThemeProviderInterface;
-use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\App\Action\Action;
+use Magento\Backend\App\Action\Context;
 
-class DeployStatic extends \Magento\Backend\Controller\Adminhtml\Cache
+class DeployStatic extends Action
 {
-
     /**
-     * @var \Magento\Framework\App\Cache\TypeListInterface
-     */
-    protected $_cacheTypeList;
-
-    /**
-     * @var \Magento\Framework\App\Cache\StateInterface
-     */
-    protected $_cacheState;
-
-    /**
-     * @var \Magento\Framework\App\Cache\Frontend\Pool
-     */
-    protected $_cacheFrontendPool;
-
-    /**
-     * @var \Magento\Framework\View\Result\PageFactory
-     */
-    protected $resultPageFactory;
-
-    /**
-     * @var \WeltPixel\Command\Model\GenerateCss
-     */
-    protected $generateCss;
-
-    /**
-     * @var ScopeConfigInterface
-     */
-    protected $scopeConfig;
-
-    /** @var  ThemeProviderInterface */
-    protected $themeProvider;
-
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $_storeManager;
-
-    /**
-     * Store collection
-     * storeId => \Magento\Store\Model\Store
-     *
-     * @var array
-     */
-    protected $_storeCollection;
-
-    /**
-     * @var \Magento\Framework\View\Asset\MergeService
-     */
-    protected $mergeService;
-
-    /**
-     * @var \Magento\Framework\App\Cache\Manager
-     */
-    protected $cacheManager;
-
-
-    /**
-     * GenerateCss constructor.
-     * @param Action\Context $context
-     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
-     * @param \Magento\Framework\App\Cache\StateInterface $cacheState
-     * @param \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
-     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
-     * @param \WeltPixel\Command\Model\GenerateCss $generateCss
-     * @param ScopeConfigInterface $scopeConfig
-     * @param ThemeProviderInterface $themeProvider
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Framework\View\Asset\MergeService $mergeService
-     * @param \Magento\Framework\App\Cache\Manager $cacheManager
+     * @param Context $context
      */
     public function __construct(
-        Action\Context $context,
-        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
-        \Magento\Framework\App\Cache\StateInterface $cacheState,
-        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \WeltPixel\Command\Model\GenerateCss $generateCss,
-        ScopeConfigInterface $scopeConfig,
-        ThemeProviderInterface $themeProvider,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\View\Asset\MergeService $mergeService,
-        \Magento\Framework\App\Cache\Manager $cacheManager
+        Context $context
     ) {
-        parent::__construct($context, $cacheTypeList, $cacheState, $cacheFrontendPool, $resultPageFactory);
-        $this->_cacheTypeList = $cacheTypeList;
-        $this->_cacheState = $cacheState;
-        $this->_cacheFrontendPool = $cacheFrontendPool;
-        $this->resultPageFactory = $resultPageFactory;
-        $this->generateCss = $generateCss;
-        $this->scopeConfig = $scopeConfig;
-        $this->themeProvider = $themeProvider;
-        $this->_storeManager = $storeManager;
-        $this->mergeService = $mergeService;
-        $this->cacheManager = $cacheManager;
-        $this->_storeCollection = $storeManager->getStores();
+        parent::__construct($context);
     }
 
     /**
-     * Generate the css files from admin button trigger
-     *
      * @return \Magento\Backend\Model\View\Result\Redirect
      */
     public function execute()
     {
-        $params = $this->getRequest()->getPost();
-
         $command = "echo deploy > /home/mage2user/site/current/pub/media/deploy1.flag";
         $data = "<pre>".shell_exec($command)."</pre>";
 
         $message = "Regenerate static files has been executed, your site will be in maintenance mode in few minutes.";
-
         $this->messageManager->addSuccess(__($message));
 
-        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
-        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        return $resultRedirect->setPath('adminhtml/*');
+        return $this->_redirect('customstyle/cache/index');
     }
 }
