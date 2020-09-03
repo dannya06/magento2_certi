fefb0a01b3988b49f0a29f5c75ddf22fdfd4f92b
yohansyahck <yohansyah@icube.us>
2020-09-03 07:32:52 +0000

    Add checking authorization rule existence

--- app/code/Icube/Brands/Controller/Adminhtml/Items.php
+++ app/code/Icube/Brands/Controller/Adminhtml/Items.php
@@ -28,23 +28,39 @@ abstract class Items extends \Magento\Backend\App\Action
     protected $resultPageFactory;
 
     /**
+     * @var \Magento\Authorization\Model\Acl\AclRetriever
+     */
+    protected $aclRetriever;
+
+    /**
+     * @var \Magento\Backend\Model\Auth\Session
+     */
+    protected $authSession;
+
+    /**
      * Initialize Group Controller
      *
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\Registry $coreRegistry
      * @param \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
      * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \Magento\Authorization\Model\Acl\AclRetriever $aclRetriever
+     * @param \Magento\Backend\Model\Auth\Session $authSession
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
         \Magento\Framework\Registry $coreRegistry,
         \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Authorization\Model\Acl\AclRetriever $aclRetriever,
+        \Magento\Backend\Model\Auth\Session $authSession
     ) {
         $this->_coreRegistry = $coreRegistry;
         parent::__construct($context);
         $this->resultForwardFactory = $resultForwardFactory;
         $this->resultPageFactory = $resultPageFactory;
+        $this->aclRetriever = $aclRetriever;
+        $this->authSession = $authSession;
     }
 
     /**
@@ -66,6 +82,17 @@ abstract class Items extends \Magento\Backend\App\Action
      */
     protected function _isAllowed()
     {
-        return $this->_authorization->isAllowed('Icube_Brands::items');
+        $user = $this->authSession->getUser();
+        $role = $user->getRole();
+        $resources = $this->aclRetriever->getAllowedResourcesByRole($role->getId());
+        if (!in_array('Icube_Brands::items', $resources)) {
+            if (in_array('Magento_Backend::all', $resources)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return $this->_authorization->isAllowed('Icube_Brands::items');
+        }
     }
 }
