64177133e2608b4fd00c0be1b838e92ed3879c09
ICUBE Tech Team <techlead-icube@sirclo.com>
2021-06-09 10:23:27 +0700

    Revert "fix[OutOfStock] Out of Stock - wrong stock_id status availability"

--- app/code/Icube/CustomSalable/Helper/Data.php
+++ app/code/Icube/CustomSalable/Helper/Data.php
@@ -5,37 +5,28 @@ namespace Icube\CustomSalable\Helper;
 use Magento\Framework\App\Helper\Context;
 use Magento\InventorySalesApi\Api\GetProductSalableQtyInterface;
 use Magento\Catalog\Model\ProductRepository;
-use Magento\Framework\App\ResourceConnection;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {   
     protected $stockState;
     protected $productRepository;
-    protected $resourceConnection;
 
     public function __construct(
         Context $context,
         GetProductSalableQtyInterface $stockState,
-        ProductRepository $productRepository,
-        ResourceConnection $resourceConnection
+        ProductRepository $productRepository
     ){
         $this->stockState = $stockState ;
         $this->productRepository = $productRepository;
-        $this->resourceConnection = $resourceConnection;
         parent::__construct($context);
     }
 
     public function isSalable($_product)
     {
-        $connection = $this->resourceConnection->getConnection();
-        $query = "SELECT stock_id FROM inventory_stock_sales_channel";
-        $result = $connection->fetchAll($query);
-        $stockId = $result[0]['stock_id'];
-
         if ($_product->getTypeId() == 'configurable') {
             $_children = $_product->getTypeInstance()->getUsedProducts($_product);
             foreach ($_children as $child) {
-                $qty = $this->stockState->execute($child->getSku(), $stockId);
+                $qty = $this->stockState->execute($child->getSku(), 1);
                 if ($qty > 0) {
                     return true;
                 }
@@ -48,7 +39,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
                 $salable[$optId] = false;
                 foreach ($options as $childId) {
                     $child = $this->productRepository->getById($childId);
-                    $qty = $this->stockState->execute($child->getSku(), $stockId);
+                    $qty = $this->stockState->execute($child->getSku(), 1);
                     if ($qty > 0) {
                         $salable[$optId] = true;
                     }
@@ -61,7 +52,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             }
 
         } else {
-            $qty = $this->stockState->execute($_product->getSku(), $stockId);
+            $qty = $this->stockState->execute($_product->getSku(), 1);
             if ($qty > 0) {
                 return true;
             }
