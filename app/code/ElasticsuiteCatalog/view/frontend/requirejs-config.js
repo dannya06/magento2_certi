2765bcd397eacfff1794febc72e12ed56a237517
hanifrizalpratama <hanif@icube.us>
2021-06-11 01:38:27 +0000

    fix([26797271] Category - Filter): fixing decimal price range

---
+++ app/code/ElasticsuiteCatalog/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Smile_ElasticsuiteCatalog/js/range-slider-widget': {
+                'Icube_ElasticsuiteCatalog/js/range-slider-widget-mixin': true
+            }
+        }
+    }
+};
