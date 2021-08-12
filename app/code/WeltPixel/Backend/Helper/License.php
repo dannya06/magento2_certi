63a9c4691f65c7926af37eeb9d3d3005050f4212
hanifrizalpratama <hanif@icube.us>
2021-08-12 01:22:26 +0000

    feat([26666923] Weltpixel - Backend: Hack)

--- app/code/WeltPixel/Backend/Helper/License.php
+++ app/code/WeltPixel/Backend/Helper/License.php
@@ -50,6 +50,7 @@ class License extends \Magento\Framework\App\Helper\AbstractHelper
     public function getMdsL()
     {
         return $this->license->getMdsL();
+        #return true;
     }
 
     /**
@@ -74,7 +75,8 @@ class License extends \Magento\Framework\App\Helper\AbstractHelper
      */
     public function isLcVd($license, $module)
     {
-        return $this->license->isLcVd($license, $module);
+        #return $this->license->isLcVd($license, $module);
+        return true;
     }
 
 
