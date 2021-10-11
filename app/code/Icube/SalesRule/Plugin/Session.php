5367d875dce5692998e14ad235ee36e1ac742540
Hafizh Pratama <hafizh.pratama@sirclo.com>
2021-10-11 09:19:18 +0000

    fix([26969377] SalesRule - Discount): remove controller

--- app/code/Icube/SalesRule/Plugin/Session.php
+++ app/code/Icube/SalesRule/Plugin/Session.php
@@ -41,30 +41,6 @@ class Session
         $connection = $this->resource->getConnection(\Magento\Framework\App\ResourceConnection::DEFAULT_CONNECTION);
         $tableName = $this->resource->getTableName('quote');
         
-        // $applied_rule_ids = $quote->getData("applied_rule_ids");
-  
-        // $applied_rule_ids = explode(",",$applied_rule_ids);
-        // foreach($applied_rule_ids as $applied_rule_id){
-        //     $sales_rule_id = $this->salesRule->load($applied_rule_id);
-        //     if($applied_rule_id == $sales_rule_id->getData("rule_id")){
-        //         if($sales_rule_id->getData("is_active") == 0){
-        //             if(is_array($applied_rule_ids)){
-        //                 $applied_rule_ids = array_diff($applied_rule_ids, [$applied_rule_id]);
-        //                 $applied_rule_ids = implode(",", $applied_rule_ids);
-        //                 if(!empty($applied_rule_ids)){
-        //                     $sql = "UPDATE " . $tableName . " SET applied_rule_ids='".$applied_rule_ids."' WHERE entity_id = '".$quote->getData("entity_id")."'";$connection->query($sql);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/whislist.log');
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $logger->info("tes masuk 1");
-
-
         $sql = "UPDATE " . $tableName . " SET trigger_recollect=0 WHERE entity_id = '".$quote->getData("quote_id_1")."'";$connection->query($sql);
 
     }
