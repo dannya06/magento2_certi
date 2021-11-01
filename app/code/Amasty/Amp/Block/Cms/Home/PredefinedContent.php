4722ef399a39bd09bd5fa1a613dcaf8ce7c29487
Yogi Teza <yogi.teza@mail.sirclo.com>
2021-11-01 09:49:52 +0000

    Improve([26930440] Extension version updates): Adding required module for Amasty Page Speed Optimizer

---
+++ app/code/Amasty/Amp/Block/Cms/Home/PredefinedContent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Amasty\Amp\Block\Cms\Home;
+
+class PredefinedContent extends \Magento\Framework\View\Element\Template
+{
+    protected $_template = 'Amasty_Amp::cms/home/ampcontent.phtml';
+}
