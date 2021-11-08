436fca3ddc45c079fadf5a0802e1d9547d712bdb
Thaariq Prasz <muhammad.thaariq@sirclo.com>
2021-11-08 01:04:14 +0000

    Fix[27027504] Order Comment : Can't place order

---
+++ app/code/Icube/OrderComment/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = 
+{
+    map: 
+    {
+        '*': 
+        {
+            'Bold_OrderComment/js/model/checkout/order-comment-validator':'Icube_OrderComment/js/model/checkout/order-comment-validator'
+        }
+    }
+};
