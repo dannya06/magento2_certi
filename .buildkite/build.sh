f091d9a781853c4983f73f7cc4b37afaa367e996
elasyaf <ananda.putra@sirclo.com>
2022-01-14 11:35:16 +0700

    Update

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -6,10 +6,12 @@ fi
 
 # build docker image
 ts=$(date +%s)
-githead=$(git rev-parse HEAD)
+githead=$(git rev-list -1 tags/$SWIFT_TAG)
 set -ex
 echo "Building Backend Docker image with tag " $$githead-$$ts
 echo $githead-$ts | buildkite-agent meta-data set "image-tag" 
 docker login -u _json_key -p "$(cat /etc/service_key/service-account-key.json | tr '\n' ' ')" https://asia.gcr.io
-docker build --build-arg VCS_REF=$(git rev-list -1 tags/$SWIFT_TAG) -t asia.gcr.io/sirclo-iii-nonprod/$1:$SWIFT_TAG -f Dockerfile.swift24 .
+git checkout tags/$SWIFT_TAG
+#docker build --build-arg VCS_REF=$(git rev-list -1 tags/$SWIFT_TAG) -t asia.gcr.io/sirclo-iii-nonprod/$1:$SWIFT_TAG -f Dockerfile.swift24 .
+docker build -t asia.gcr.io/sirclo-iii-nonprod/$1:$SWIFT_TAG -f Dockerfile.swift24 .
 docker push asia.gcr.io/sirclo-iii-nonprod/$1:$SWIFT_TAG
