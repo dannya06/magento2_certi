bdc1edee77a258ec0cfacc405b0e53b3a7c8ac87
hanifrizalpratama <56011495+hanifrizalpratama@users.noreply.github.com>
2022-08-18 11:24:55 +0700

    Update global_patch.sh

--- global_patch.sh
+++ global_patch.sh
@@ -5,5 +5,6 @@ cd /home/app/site
 # patch security email
 patch -p1 --forward < m2_hotfixes/MDVA-43395_EE_2.4.3-p1_COMPOSER_v1.patch || true
 patch -p1 --forward < m2_hotfixes/MDVA-43443_EE_2.4.2-p2_COMPOSER_v1.patch || true
+patch -p1 --forward < m2_hotfixes/0001-fix-27332816-bug-flushing-cache-fpc-configiration-ca.patch || true
 
 ######################### END of line patch ##############################
